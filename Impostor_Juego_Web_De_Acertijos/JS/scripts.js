7441038ff3bc0db3ce908b676b91095fc088f302
Z8-28 <114176071+Z8-28@users.noreply.github.com>
2025-11-13 16:09:55 -0600

    Interface
    Mejora de interface

--- Impostor_Juego_Web_De_Acertijos/JS/scripts.js
+++ Impostor_Juego_Web_De_Acertijos/JS/scripts.js
@@ -1,3 +1,12 @@
 function goto_config(){
     window.location.href = "HTML/config.html";
 }
+
+function ajustarPantalla() {
+    const altura = window.visualViewport?.height || window.innerHeight;
+    document.documentElement.style.setProperty('--real-height', altura + 'px');
+}
+
+window.addEventListener('resize', ajustarPantalla);
+window.addEventListener('orientationchange', ajustarPantalla);
+ajustarPantalla();
